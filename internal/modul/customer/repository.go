88d05b31baac5c59e80c3dc6a1dbde841eba3d13
Kukuh Rahmadani <krahmadani1@gmail.com>
2025-05-03 21:59:56 +0700

    Repository customer

---
+++ internal/modul/customer/repository.go
@@ -0,0 +1,73 @@
+package customer
+
+import (
+	"context"
+	"database/sql"
+	"github.com/doug-martin/goqu/v9"
+	"kukuhkkh.id/learn/bengkel/domain"
+)
+
+type repository struct {
+	db *goqu.Database
+}
+
+func NewRepository(con *sql.DB) domain.CustomerRepository {
+	return &repository{
+		db: goqu.New("default", con),
+	}
+}
+
+func (r repository) FindByID(ctx context.Context, id int) (customer domain.Customer, err error) {
+	dataset := r.db.From("customers").Where(goqu.Ex{
+		"id": id,
+	})
+
+	_, err = dataset.ScanStructContext(ctx, &customer)
+	if err != nil {
+		return domain.Customer{}, err
+	}
+
+	return
+}
+
+func (r repository) FindByIds(ctx context.Context, ids []int) (customers []domain.Customer, err error) {
+	dataset := r.db.From("customers").Where(goqu.Ex{
+		"id": ids,
+	})
+
+	err = dataset.ScanStructsContext(ctx, &customers)
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (r repository) FindByPhone(ctx context.Context, phone string) (customer domain.Customer, err error) {
+	dataset := r.db.From("customers").Where(goqu.Ex{
+		"phone": phone,
+	})
+
+	_, err = dataset.ScanStructContext(ctx, &customer)
+	if err != nil {
+		return domain.Customer{}, err
+	}
+
+	return
+}
+
+func (r repository) Insert(ctx context.Context, customer *domain.Customer) error {
+	executor := r.db.Insert("customers").Rows(*customer).Executor()
+
+	res, err := executor.ExecContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+	customer.ID = int(id)
+	return err
+}
