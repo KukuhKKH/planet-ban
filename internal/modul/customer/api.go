84473fb3ea547618e0eadcbe406aebb6fcee985f
Kukuh Rahmadani <krahmadani1@gmail.com>
2025-05-04 14:28:23 +0700

    Aksi save customer

--- internal/modul/customer/api.go
+++ internal/modul/customer/api.go
@@ -18,6 +18,7 @@ func NewApi(app *fiber.App, customerService domain.CustomerService) {
 	}
 
 	app.Get("/v1/customer", api.AllCustomers)
+	app.Post("/v1/customer", api.SaveCustomer)
 }
 
 func (a api) AllCustomers(ctx *fiber.Ctx) error {
@@ -25,6 +26,30 @@ func (a api) AllCustomers(ctx *fiber.Ctx) error {
 	defer cancel()
 
 	apiResponse := a.customerService.ALl(c)
+	util.ResponseInterceptor(c, &apiResponse)
+
+	return ctx.Status(200).JSON(apiResponse)
+}
+
+func (a api) SaveCustomer(ctx *fiber.Ctx) error {
+	c, cancel := context.WithTimeout(ctx.Context(), 30*time.Second)
+	defer cancel()
+
+	var customerData domain.CustomerData
+
+	if err := ctx.BodyParser(&customerData); err != nil {
+		apiResponse := domain.ApiResponse{
+			Code:    "400",
+			Message: "Bad Request",
+			Data:    nil,
+		}
+
+		util.ResponseInterceptor(c, &apiResponse)
+
+		return ctx.Status(400).JSON(apiResponse)
+	}
+
+	apiResponse := a.customerService.Save(c, customerData)
 	util.ResponseInterceptor(c, &apiResponse)
 
 	return ctx.Status(200).JSON(apiResponse)
